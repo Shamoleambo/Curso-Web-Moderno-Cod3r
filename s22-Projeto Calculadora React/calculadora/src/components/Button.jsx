0144783074a811e7529974f973f36904b940497f
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-04 09:16:34 -0300

    Criacao Componente Display e Display.css; Criacao das funcoes eaplicacao do estado; Termino da secao 22

--- s22-Projeto Calculadora React/calculadora/src/components/Button.jsx
+++ s22-Projeto Calculadora React/calculadora/src/components/Button.jsx
@@ -1,6 +1,13 @@
 import './Button.css';
 
 export default props => 
-    <button className="button">
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}
+        onClick={e => props.click && props.click(props.label)}
+    >
         {props.label}
     </button>
---
+++ s22-Projeto Calculadora React/calculadora/src/components/Display.jsx
@@ -0,0 +1,6 @@
+import './Display.css';
+
+export default props => 
+    <div className="display">
+        {props.value}
+    </div>
