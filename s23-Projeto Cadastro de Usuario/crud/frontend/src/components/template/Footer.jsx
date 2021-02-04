52f4c59991143ef6c3ec90d8969392a0dad42839
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-04 11:46:11 -0300

    Termino do layout do Footer e de seu CSS

--- s23-Projeto Cadastro de Usuario/crud/frontend/src/components/template/Footer.jsx
+++ s23-Projeto Cadastro de Usuario/crud/frontend/src/components/template/Footer.jsx
@@ -1,6 +1,9 @@
-import './Footer.css';
+import "./Footer.css";
 
-export default props =>
-    <footer className="footer">
-        Rodapé
-    </footer>
+export default (props) => (
+  <footer className="footer">
+    <span> Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong>Cod<span className="text-danger">3</span>r</strong>
+    </span>
+  </footer>
+);
