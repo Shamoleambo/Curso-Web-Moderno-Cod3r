f17c1cd39001956c685f2c4974cb7baae3568cb5
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-05 08:36:37 -0300

    Criacao do estado inicial de usuarioe metodos: clear, getUpdatedList e save

--- s23-Projeto Cadastro de Usuario/crud/frontend/src/components/user/UserCrud.jsx
+++ s23-Projeto Cadastro de Usuario/crud/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import axios from 'axios';
 import Main from '../template/Main';
 
 const headerProps = {
@@ -7,8 +8,40 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Icluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users';
+const initialState = {
+    user: {name: '', email: ''},
+    list: []
+}
+
 export default class UserCrud extends Component {
     /*Challenge: Refactor this code separating the code in components*/
+
+    state = {...initialState};
+
+    //Clear the form
+    clear(){
+        this.setState({user: initialState.user});
+    }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter(u => u.id !== user.id);
+        list.unshift(user);
+        return list;
+    }
+
+    //Insert a new user and updates an already existing user
+    save(){
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+        axios[method](url, user)
+            .then(resp =>{
+                const list = this.getUpdatedList(resp.data);
+                this.setState({user: initialState.user, list});
+            });
+    }
+
     render(){
         return(
             <Main {...headerProps}>
