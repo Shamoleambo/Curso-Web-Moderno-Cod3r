75da0579fd3ce1538e0c0570533245bfea617f51
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-11 14:25:04 -0300

    Finalizacao do projeto; Estilizacao das portas; Finalizacao da logica

--- s27-Projeto Monty Hall Vue/montyhall/src/App.vue
+++ s27-Projeto Monty Hall Vue/montyhall/src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <h1>Problema de Monty Hall</h1>
         <div class="form">
-            <div v-if="!stated">
+            <div v-if="!started">
                 <label for="doorAmount">Quantas portas?</label>
                 <input type="text" id="doorAmount" size="3"
                     v-model.number="doorAmount">
@@ -14,6 +14,11 @@
             </div>
             <button v-if="!started" @click="started = true" >Iniciar</button>
             <button v-if="started" @click="started = false" >Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in doorAmount" :key="i" >
+                <Door :hasGift="i === selectedDoor" :number="i" />
+            </div>
         </div>
     </div>
 </template>
@@ -58,4 +63,25 @@ body{
     padding: 20px;
     margin-bottom: 60px;
 }
+
+.form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form.input, .form button{
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors{
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+
 </style>
