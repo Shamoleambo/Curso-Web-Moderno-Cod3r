e38314ccc1b0c2b59e49f77fe604e9e0017748b9
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-15 13:45:37 -0300

    Criacao do agrupamento das palavras

--- s28-Electron.js - Most Used Words/most-used-words/src/backend/index.js
+++ s28-Electron.js - Most Used Words/most-used-words/src/backend/index.js
@@ -2,21 +2,12 @@ const { ipcMain } = require("electron");
 
 const pathsToRows = require('./pathsToRows');
 const prepareData = require('./prepareData');
+const groupWords = require('./groupWords');
 
 ipcMain.on("process-subtitles", (event, paths) => {
   pathsToRows(paths)
     .then(rows => prepareData(rows))
     .then(words => console.log(words))
-    .then(() =>{
-        event.reply("process-subtitles", [
-          { name: "i", amount: 1234 },
-          { name: "you", amount: 900 },
-          { name: "he", amount: 800 },
-          { name: "she", amount: 799 },
-          { name: "our", amount: 200 },
-          { name: "house", amount: 2 },
-          { name: "main", amount: 1300 }
-        ]);
-    });
+    .then(groupedWords => event.reply("process-subtitles", groupedWords));
 
 });
