65cd833df8ac838868532c5a54c650c1307b5010
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-15 12:06:57 -0300

    Continuacao do processamento de arquivos

--- s28-Electron.js - Most Used Words/most-used-words/src/backend/prepareData.js
+++ s28-Electron.js - Most Used Words/most-used-words/src/backend/prepareData.js
@@ -1,10 +1,32 @@
 module.exports = rows =>{
     return new Promise((resolve, reject) => {
         try{
-            const words
+            const words = rows
+                .filter(filterValidRow)
+                .map(removePunctuation)
+                .map(removeTags)
+                .reduce(mergeRows)
+                .split(' ')
+                .map(word => word.toLowerCase())
+                .map(word => word.replace('"', ''));
+
             resolve(words);
         }catch(e){
             reject(e);
         }
     });
 }
+
+function filterValidRow(row){
+    //It will verify if what was parsed was not a number
+    const notNumber = !parseInt(row.trim());
+    //Removes blank spaces from lines and set those empty lines as false, or true if they are not empty
+    const notEmpty = !!row.trim();
+    //Not time stamps of the subtitles;
+    const notInterval = !row.includes('-->');
+    return  notNumber && notEmpty && notInterval;
+}
+
+const removePunctuation = row => row.replace(/[,?!.-]/g, '');
+const removeTags = row => row.replace(/(<[^>]+)>/ig, '').trim();
+const mergeRows = (fullText, row) => `${fullText} ${row}`;
