6925e87ed02e1002ae9d1a4b6458a9b2e4189c03
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-18 15:48:14 -0300

    Implementacao da funcionalidade delete

---
+++ s29-Angular & Projeto/crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,45 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(
+    private poductService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.poductService
+      .readById(id)
+      .subscribe(product => this.product = product);
+  }
+
+  deleteProduct():void{
+    this.poductService
+      .delete(this.product.id)
+      .subscribe(product =>{
+        this.poductService.showMessage("Produto Deletado com Sucesso!");
+      });
+
+    this.router.navigate(['/products']);
+  }
+
+  cancel(): void{
+    
+  }
+
+}
