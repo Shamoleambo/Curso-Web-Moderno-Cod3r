4e97a5109fa752e25b513927ee7bcca3aff61c69
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-22 07:14:55 -0300

    Testando requisicoes no backend

--- s32-Express/index.js
+++ s32-Express/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=> console.log("eitaa!"));
+app.get('/opa',(req, res)=>{
+    res.send('Estou <strong>bem!</strong>');
+});
+
+app.listen(3000, ()=> console.log("Backend executando..."));
