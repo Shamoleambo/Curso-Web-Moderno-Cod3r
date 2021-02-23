0cb15f331b5cc3db13a6a8cb5065adbfc191a216
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-23 07:58:23 -0300

    Criacao do knexfile.js contendo as configs do db; Criacao do db.js em config que usa o knexfile e o knex para configurar o db e o exportar, com o import ocorrendo no index.js; Criacao de 3 migrations usando o knex migrate:make

--- sFINAL - Projeto Knowledge/backend/index.js
+++ sFINAL - Projeto Knowledge/backend/index.js
@@ -1,5 +1,8 @@
 const app = require('express')(); //The result of the function called by express is inside app
 const consign = require('consign');
+const db = require('./config/db'); //This is the knex with de db config(knexfile.js) exported by db.js in the config directory
+
+app.db = db;
 
 consign()
     .then('./config/middlewares.js')
