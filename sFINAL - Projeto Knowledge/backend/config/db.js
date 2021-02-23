6ba17209a1eaab58e868fb76ac6f1bbcf99b3636
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-23 08:53:29 -0300

    Determinacao dos campos de cada migration e suas relacoes entre si para a evolucao do db(com o up) ou rollback(com o down); Correcao de algumas dependencias no package.json; Adicao das migrations serem carregadas automaticamente quando o backend for executado

--- sFINAL - Projeto Knowledge/backend/config/db.js
+++ sFINAL - Projeto Knowledge/backend/config/db.js
@@ -1,4 +1,5 @@
 const config = require('../knexfile');
 const knex = require('knex')(config);
 
+knex.migrate.latest([config]); //This command will load the tables in the moment when you execute the backend
 module.exports = knex
