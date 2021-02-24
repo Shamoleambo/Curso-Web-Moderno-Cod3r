ac35d5e7b36372b1b847586d70800ac3d4335840
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-24 15:58:42 -0300

    Implementacao dos article.js e seus metodos assim como suas rotas

--- sFINAL - Projeto Knowledge/backend/config/routes.js
+++ sFINAL - Projeto Knowledge/backend/config/routes.js
@@ -10,14 +10,23 @@ module.exports = app =>{
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save);
-
+ 
     app.route('/categories/tree')
         .get(app.api.category.getTree);
-        
-    //This is a more generic url, it should come at the bottom
+
+    //This is a more generic url, it should come at the bottom to not interfere with the more specific urls above
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove);
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+    
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove);
 
 }
