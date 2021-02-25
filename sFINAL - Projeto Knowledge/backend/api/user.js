9e288b6daf26be243bb8a986f5d33821ff7a6e78
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-25 12:14:36 -0300

    Validacao de admins implementadas

--- sFINAL - Projeto Knowledge/backend/api/user.js
+++ sFINAL - Projeto Knowledge/backend/api/user.js
@@ -16,6 +16,9 @@ module.exports = app =>{
         const user = {...req.body};
         if(req.params.id) user.id = req.params.id;
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false;
+        if(!req.user || !req.user.admin) user.admin = false;
+
         //Validations:
         try{
             existsOrError(user.name, "Nome não informado");
