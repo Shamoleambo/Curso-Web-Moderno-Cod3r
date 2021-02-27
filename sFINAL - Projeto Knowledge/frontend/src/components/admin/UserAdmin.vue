0a7df3b898de2d672af5ae81d6be7a0b5d8adee4
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-27 09:24:39 -0300

    Uso de b-table para renderizar os usuarios no componente UserAdmin

--- sFINAL - Projeto Knowledge/frontend/src/components/admin/UserAdmin.vue
+++ sFINAL - Projeto Knowledge/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,43 @@
 <template>
     <div class="user-admin">
-        <h1>Usuário Componente</h1>
+        <b-table hover striped :items="users" :fields="fields" >
+
+        </b-table>
     </div>
 </template>
 
 <script>
+import { baseApiUrl } from '@/global';
+
+import axios from 'axios';
+
 export default {
-    name: 'UserAdmin'
+    name: 'UserAdmin',
+    data: function(){ //The data attribute is a function that returns an object
+        return {
+            mode: 'save',
+            user: {},
+            users: [],
+            fields: [
+                {key: 'id', label: 'Código', sortable: true},
+                {key: 'name', label: "Nome", sortable: true},
+                {key: 'email', label: "E-mail", sortable: true},
+                {key: 'admin', label: "Administrador", sortable: true, 
+                    formater: value => value ? 'Sim' : "Não"},
+                {key: 'actions', label: 'Ações'}
+            ]
+        };
+    },
+    methods: {
+        loadUsers() {
+            const url = `${baseApiUrl}/users`;
+            axios.get(url)
+                .then(res => this.users = res.data);
+        }
+    },
+    mounted(){ //<=Lifecycle method
+        this.loadUsers();
+    }
 }
 </script>
 
