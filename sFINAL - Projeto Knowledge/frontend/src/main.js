b9f13490bbc67f6ad0c83c41400ba2dc625d472e
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-02-27 09:49:13 -0300

    Configurando o Vue toasted

--- sFINAL - Projeto Knowledge/frontend/src/main.js
+++ sFINAL - Projeto Knowledge/frontend/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 import App from './App';
 
 import './config/bootstrap'; //There is no need to pass the bootstrap to the Vue instance, all you got to do is to load it
+import './config/msgs'; //The same as above
+
 import store from './config/store';
 import router from './config/router';
 
