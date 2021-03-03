e2d1ffae6356ec160759279fff2e96672a3c6b22
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-03-03 09:17:47 -0300

    Implementacao da arvore de categorias no menu e sua estilizacao

--- sFINAL - Projeto Knowledge/frontend/src/components/template/Menu.vue
+++ sFINAL - Projeto Knowledge/frontend/src/components/template/Menu.vue
@@ -1,13 +1,44 @@
 <template>
-    <aside class="menu" v-show="isMenuVisible" ></aside>
+    <aside class="menu" v-show="isMenuVisible" >
+        <div class="menu-filter">
+            <i class="fa fa-search fa-lg"></i>
+            <input type="text" placeholder="Digite para filtrar..."
+                v-model="treeFilter" class="filter-field" />
+        </div>
+        <Tree :data="treeData"
+            :options="treeOptions"
+            ref="tree" :filter="treeFilter" />
+    </aside>
 </template>
 
 <script>
 import { mapState } from 'vuex'; //mapState is responsible to map store's attributes inside the component
+import Tree from 'liquor-tree';
+import axios from 'axios';
+
+import { baseApiUrl } from '@/global';
 
 export default {
     name: 'Menu',
-    computed: mapState(['isMenuVisible']) //isMenuVisible is the attribute being mapped inside the Menu Component
+    components: { Tree },
+    computed: mapState(['isMenuVisible']), //isMenuVisible is the attribute being mapped inside the Menu Component
+    data: function(){
+        return {
+            treeFilter: '',
+            treeData: this.getTreeData(),
+            treeOptions: {
+                propertyNames: {'text': 'name'},
+                filter: {emptyText: 'Categoria não encontrada'}
+            }
+        };
+    },
+    methods: {
+        getTreeData(){
+            const url = `${baseApiUrl}/categories/tree`;
+
+            return axios.get(url).then(res => res.data);
+        }
+    }
 }
 </script>
 
@@ -20,4 +51,52 @@ export default {
         flex-direction: column;
         flex-wrap: wrap;
     }
+
+    .menu a,
+    .menu a:hover {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .menu .tree-node.selected > .tree-content,
+    .menu .tree-node .tree-content:hover{
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    .tree-arrow.has-child {
+        filter: brightness(2);
+    }
+
+    .menu .menu-filter{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin: 20px;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #aaa;
+    }
+
+    .menu .menu-filter i{
+        color: #aaa;
+        margin-right: 10px;
+    }
+
+    .menu input{
+        color: #ccc;
+        font-size: 1.3rem;
+        border: 0;
+        outline: 0;
+        width: 100%;
+        background: transparent;
+    }
+
+    .tree-filter-empty{
+        color: #ccc;
+        margin: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
 </style>
